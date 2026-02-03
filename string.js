c781e7b608bc431ccd0a22c690b277c7d88c4733
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-02-03 10:21:07 +0600

    modified the file

--- string.js
+++ string.js
@@ -2,3 +2,21 @@ const name = 'Tisha';
 const country = "Bangladesh";
 const district = `Brahmanbaria`;
 const upuzila = new String('B-baria');
+
+console.log(typeof name);
+console.log(typeof upuzila);
+console.log(upuzila);
+//Array------->mutable,changeable
+const num = [20, 30, 45, 38, 23];
+console.log(num);
+// console.log(num.length);
+// console.log(num[0]);
+num[1] = 33;
+console.log(num);
+
+//String------->immutable,not changeable
+const capital = 'Dhaka';
+console.log(capital.length);
+console.log(capital[2]);
+capital[0] = 'f';
+console.log(capital);
