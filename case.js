efb8404f7a0d534d0499091dc8193494b9a991f3
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-02-03 11:57:29 +0600

    added case.js

---
+++ case.js
@@ -0,0 +1,26 @@
+const school = 'Sabera Subhan Govt Girls High School';
+console.log(school);
+console.log(school.toLowerCase());
+console.log(school.toUpperCase());
+
+const book = 'chemistry';
+const subject = 'Chemistry';
+
+
+if(book.toLowerCase() === subject.toLowerCase()){
+    console.log('dhomaiya porbu');
+}
+else{
+    console.log('hodhai boy ar prishta ultabu');
+}
+
+
+const drink = 'water';
+const liquid = ' water';
+
+if(drink.trim() === liquid.trim()){
+    console.log('water is life')
+}
+else{
+    console.log('pani khaiyo nah');
+}
