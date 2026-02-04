9c47dac0247b8a3476346445e17d425d70997aa9
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-02-04 13:08:10 +0600

    added concat.js file

---
+++ concat.js
@@ -0,0 +1,20 @@
+const first = 'Masuda';
+//const space = ' ';
+const last = 'Tisha';
+
+const result =first.concat(' ').concat(' ',last);
+console.log(result);
+
+const firstName = 'sefali';
+const lastName = 'malaika';
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+//includes
+ const thana = 'nabinogor';
+ console.log(thana.includes('g'));
+ console.log(thana[3]);
+ console.log(thana.indexOf('b'));
+
+
+
