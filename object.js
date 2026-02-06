706f1ba936288f0b050323b7c1a91148c6e2487a
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-02-06 13:38:37 +0600

    added object.js file

---
+++ object.js
@@ -0,0 +1,21 @@
+//primitive / basic /primary
+const studentName = 'mila';
+const age = 30;
+const isbrilliant = true;
+
+//non-primitive / complex data
+const exams = ['bangla', 'english'];
+
+//object declare.........primitive data type
+const student = {
+    name: 'Tisha',
+    age: 23,
+    height: 5.4,
+    isGood: true,
+    favoriteFood: ['vorta', 'mach', 'kabab', 'roast'],
+    results:{
+        hsc: 'good',
+        ssc: 5.00,
+        psc:5.00,
+    }
+}
